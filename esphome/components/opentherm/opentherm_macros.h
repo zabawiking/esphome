a2956e5e7f0cfbd597ce1baede970b7c7102ba88
Jerzy Żygłowicz <jerzy.zyglowicz@doverfs.com>
2025-10-10 09:53:34 +0200

    handle timeout

--- esphome/components/opentherm/opentherm_macros.h
+++ esphome/components/opentherm/opentherm_macros.h
@@ -117,7 +117,8 @@ namespace opentherm {
 // Data processing builders
 #define OPENTHERM_MESSAGE_RESPONSE_MESSAGE(msg) case MessageId::msg:
 #define OPENTHERM_MESSAGE_RESPONSE_ENTITY(key, msg_data) this->key->publish_state(message_data::parse_##msg_data(data));
-#define OPENTHERM_MESSAGE_TIMEOUT_ENTITY(key, msg_data) this->key->publish_state(NAN);
+#define OPENTHERM_MESSAGE_TIMEOUT_SENSOR_ENTITY(key, msg_data) this->key->publish_state(NAN);
+#define OPENTHERM_MESSAGE_TIMEOUT_BINARY_SENSOR_ENTITY(key, msg_data) this->key->publish_state(false);
 #define OPENTHERM_MESSAGE_RESPONSE_POSTSCRIPT break;
 
 #define OPENTHERM_IGNORE(x, y)
